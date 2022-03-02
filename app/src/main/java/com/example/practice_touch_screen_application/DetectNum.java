bfd7a25316cafafe83ded54bed2684bad3e75c8c
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-03-03 00:12:46 +0800

    removed the ugly black outer border from the scan rectangle in the screenshot

--- app/src/main/java/com/example/practice_touch_screen_application/DetectNum.java
+++ app/src/main/java/com/example/practice_touch_screen_application/DetectNum.java
@@ -1,20 +1,33 @@
 package com.example.practice_touch_screen_application;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class DetectNum extends AppCompatActivity {
     private ImageView imageView;
     private TextView result;
+    private Interpreter interpreter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +36,42 @@ public class DetectNum extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageview);
         result = (TextView) findViewById(R.id.result);
         File file = new File(getIntent().getStringExtra("path"));
+        if(!file.exists()) return;
         imageView.setImageDrawable(Drawable.createFromPath(file.toString()));
         ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) imageView.getLayoutParams();
         params.width = ConstraintLayout.LayoutParams.MATCH_PARENT;params.height = ConstraintLayout.LayoutParams.WRAP_CONTENT;
         imageView.setLayoutParams(params);
+        result.setText("Processing...");
+        startmachinelearningmodel();
     }
     public void finishthing(View view){
         setResult(Activity.RESULT_OK);
         finish();
+    }
+    public void startmachinelearningmodel(){
+        try{
+            interpreter = new Interpreter(loadModelFile(),null);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Bitmap imageBitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(imageBitmap,28,28,false);
+        int imageSize = scaledBitmap.getRowBytes() * scaledBitmap.getHeight();
+        ByteBuffer compressedBuffer = ByteBuffer.allocateDirect(imageSize);
+        scaledBitmap.copyPixelsToBuffer(compressedBuffer);
+        Tensor outputTensor = interpreter.getOutputTensor(0);
+        TensorBuffer outputBuffer= TensorBuffer.createFixedSize(outputTensor.shape(),outputTensor.dataType());
+        interpreter.run(compressedBuffer,outputBuffer.getBuffer().rewind());
+    }
+    private MappedByteBuffer loadModelFile() throws IOException
+    {
+        AssetFileDescriptor assetFileDescriptor = this.getAssets().openFd("mnist.tflite");
+        FileInputStream fileInputStream = new FileInputStream(assetFileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = fileInputStream.getChannel();
+
+        long startOffset = assetFileDescriptor.getStartOffset();
+        long len = assetFileDescriptor.getLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,len);
+
     }
 }
